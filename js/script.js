fc7a7eb6c4c7734058c25558edffa273d74bb13d
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2024-11-01 18:33:08 -0500

    added a radius style to script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,25 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 41.836878, lng: -87.625971 },
+    zoom: 9,
   });
+  
+  // Create marker 
+  var marker = new google.maps.Marker({
+  map: map,
+  position: new google.maps.LatLng(41.836878, -87.625971),
+  animation: google.maps.Animation.DROP,
+  title: 'Illinois Institute of Technology'
+  });
+
+  // Add circle overlay and bind to marker
+  var circle = new google.maps.Circle({
+  map: map,
+  radius: 32186.9, //meters
+  fillColor: '#AAAAAA'
+  });
+  circle.bindTo('center', marker, 'position');
 }
 
 initMap();
