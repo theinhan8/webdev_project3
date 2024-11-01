d6b6e1bee574d4aec56fe513d4bd8d16987ef722
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2024-11-01 17:48:30 -0500

    Update map.html, style.css, script.js
    Added the correct structure with a new api key from google clouds to map.html, than added dimensions to the map in order for it to appear and edited style.css so that the map fit into the given space. Copied the code from the documentation page for script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-function init(){
-  alert('it works');
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+initMap();
