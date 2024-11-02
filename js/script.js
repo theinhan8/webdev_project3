339d9b787b3150ee4b9adb07faa872ac956f88fd
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2024-11-01 19:06:09 -0500

    added another style that finds your current location to script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,44 @@
-let map;
+let map, infoWindow;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 41.836878, lng: -87.625971 },
     zoom: 9,
   });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Find Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+    locationButton
+  );
   
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Current Location");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
   // Create marker 
   var marker = new google.maps.Marker({
   map: map,
@@ -25,4 +56,14 @@ async function initMap() {
   circle.bindTo('center', marker, 'position');
 }
 
-initMap();
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: Current Location Cannot Be Found."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
